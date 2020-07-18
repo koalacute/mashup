dcae0b4dc5d7f7547c9020e10c7835126f360c77
七曦 <koalacute@aliyun.com>
2020-07-18 15:55:25 +0800

    Mysql && Mybatis && Logback && AutoGenerate

--- src/main/java/com/xrikui/mashup/MashupApplication.java
+++ src/main/java/com/xrikui/mashup/MashupApplication.java
@@ -1,13 +1,15 @@
 package com.xrikui.mashup;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@MapperScan("com.xrikui.mashup.mapper")
+@PropertySource(value = "classpath:/config/*.yml", ignoreResourceNotFound = true)
 @SpringBootApplication
 public class MashupApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MashupApplication.class, args);
     }
-
 }
